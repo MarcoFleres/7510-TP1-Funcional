86485f60a74f204d047f85b758ec8eab58d655d9
Marco Fleres <mafleres@gmail.com>
2017-09-05 04:25:23 -0300

    Planteado el motor como un árbol de reglas, en que los facts también lo son. Al agregar facts del mismo tipo, se genera una cadena en la regla que compara la query con los fact uno a uno.

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -1,36 +1,90 @@
 (ns logical-interpreter)
 
 (use 'clojure.string)
+;(use '[clojure.pprint :as pprint])
 
+(defn getVerb [line]
+  (get (re-matches #"(.*?)\(.*?\)\.?$" line) 1 nil))
+
+(defn getParameters [line]
+  (if-let [token (get (re-matches #".*?\((.*?)\)\.?" line) 1 nil)]
+          (map trim (split token  #","))))
+
+(defn getFactParameterComparator
+  "Devuelve una función de comparación de los parámetros del fact"
+  [line]
+  ;(printf "Getting parameter comparator of %s\n" line)
+  (let [parameters (getParameters line)]
+       (fn[x] (do
+                ;(printf "Comparando con %s\n" (pr-str parameters))
+                (= parameters x)
+                )))) ; Función de igualdad de los parámetros del fact
+  
+
+(defn isFact? [line] (boolean (re-matches #".*?\(.*?\)\." line)))  ;FIXME corregir condición para diferenciarla de las reglas
+(defn isRule? [line] false) ;TODO implementar
+
+(defn combineFacts
+  [newFactRules oldfactRules]
+  (if (= oldfactRules nil) newFactRules
+      (fn [x] (or (oldfactRules x) (newFactRules x)))))
+
+
+(defn addFact [db line]
+  ;Llamamos verb al nombre de la función
+  ;Llamamos curCondition a la función de igualdad de los parámetros del fact
+  (let [verb (getVerb line)  ;TODO asignar el verbo (con regex de la línea)
+        curCondition (getFactParameterComparator line)  ;TODO asignar función que compara con los parámetros del fact
+        ]
+      ;(printf "Adding Fact %s: verb:%s cond: %s\n" line verb curCondition)
+      
+      ; Si no existe la regla, la creamos con la condición de igualdad con el Fact
+      ; Si existe, agregamos el Fact a la condición con una OR.
+      
+      (swap! db update-in [verb] (partial combineFacts curCondition))
+      ;(printf "DB: %s\n" @db)
+    )
+  )
+  
+  
+
+(defn parseDatabase
+  "Recibe un string con la definición de la base de datos, y devuelve un mapa de reglas."
+  [database]
+  (let [db (atom {})]
+       (doseq [line (map trim (split-lines database))]
+              
+                ;(printf "Parsing Line \"%s\": " line )
+                (cond 
+                      (isFact? line) (do 
+                                        ;(printf "Is Fact\n")
+                                        (addFact db line))
+                      (isRule? line) (do
+                                       ;(printf "Is Rule\n")
+                                       ;(addRule db line)
+                                       )
+                      ;:else (printf "Ignoring Line\n")
+                )
+       )
+    ;(printf "DB: %s\n" @db)
+    @db
+  )
+)
+  
+  
 
 (defn evaluate-query
   "Returns true if the rules and facts in database imply query, false if not. If
   either input can't be parsed, returns nil"
   [database query]
-
-  ; Por cada línea:
-  ;   Si es un fact y coinciden los párámetros, devolver true
-  ;   Si es una regla y coincide el nombre, evaluar las condiciones
-  
-  (let [
-        predicates (map trim (split-lines database))
-        [_ function-name parameters] (re-matches #"(.*?)\((.*?)\)" query)
+  (let [data (parseDatabase database)
+        query-verb (getVerb query)
+        query-parameters (getParameters query)
         ]
-      
-        (printf "Searching for rule '%s' parameters: '%s'\n" function-name parameters)
-
-        ; Buscamos algún fact que matchee la query
-        
-        (if (filter 
-                  #(re-find
-                    (re-pattern (format "^%s\\(%s\\)\\." function-name parameters))
-                    %)
-            predicates)
-          true
-          false)
-        
-        
-        )
-  
+    ;(printf "Getting Verb: %s\n" query-verb)
+    ((data query-verb) query-parameters)
+  )
 )
+
+
 
